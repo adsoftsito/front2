d098d074d10f5e17e673bd72ad1c69e5ea8b4cd9
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-11-11 13:57:12 -0600

    fixed the form

--- src/app/controllers/admin/modals/AdminAddModal.component.ts
+++ src/app/controllers/admin/modals/AdminAddModal.component.ts
@@ -2,6 +2,7 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { AdminService } from '../../../services/admin.service';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from '../../../custom-validators'
 
 @Component({
     templateUrl: './AdminAddModal.component.html',
@@ -17,7 +18,7 @@ export class NgbdModalAddAdmin{
         private formBuilder: FormBuilder) {this.createForm();}
         
         closeModal() {
-            window.location.reload();
+            //window.location.reload();
             this.activeModal.close('Modal Closed');
         }
         
@@ -28,22 +29,30 @@ export class NgbdModalAddAdmin{
         
         private createForm() {
             this.myForm = this.formBuilder.group({
-                username: ['', Validators.compose([
+                username: [null, Validators.compose([
                     Validators.maxLength(30),
                     Validators.minLength(3),
                     Validators.required ])], 
-                    email: ['', Validators.compose([
+                    email: [null, Validators.compose([
                         Validators.required,
                         Validators.email
                     ])],
-                    password: ['', Validators.compose([
+                    password: [null, Validators.compose([
                         Validators.minLength(6),
                         Validators.required,
+                        CustomValidators.patternValidator(/\d/, { hasNumber: true }),
+                        CustomValidators.patternValidator(/[A-Z]||[a-z]/, { hasCase: true }),
                     ])],
+                    confirmPassword: [null, Validators.compose([Validators.required])]
+                },
+                {
+                    validator: CustomValidators.passwordMatchValidator
                 });
             }
+
         
             
             
         }
+
         

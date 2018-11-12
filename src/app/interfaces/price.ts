924159aefd5a1b8704b3d687145774b4491ff5bc
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-11-12 13:32:09 -0600

    add tour and tickettype interfaces

--- src/app/interfaces/price.ts
+++ src/app/interfaces/price.ts
@@ -1,6 +1,6 @@
 export interface IPrice {
     id: number,
     priceAmount: number,
-    tour_id: number, //maybe an object of tour
-    ticket_type_id: number, //maybe an object of ticket type
+    tour_id: any,
+    ticket_type_id: any
 }
---
+++ src/app/interfaces/tour.ts
@@ -0,0 +1,6 @@
+export interface ITour{
+    id: number,
+    name: string,
+    description: string,
+    prices: any[] //not sure if i want this
+}

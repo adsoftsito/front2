5ba62cefbd8a17a0f27222879ed0865e115ef6ee
JCVelazco <juan.carlosvr.97@hotmail.com>
2018-11-13 20:09:41 -0600

    hagamsolo mas divertido.... Starting to create all he f and fd that i will use for tour show

--- src/app/controllers/tour/tour.component.ts
+++ src/app/controllers/tour/tour.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PurchaseService } from '../../services/purchase.service';
+import {NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from '../../modals/about/about.component';
 
 @Component({
-  selector: 'app-tour',
-  templateUrl: './tour.component.html',
-  styleUrls: ['./tour.component.scss']
+  selector: 'app-about',
+  templateUrl: './purchase.component.html',
+  styleUrls: ['./purchase.component.scss']
 })
 export class TourComponent implements OnInit {
 
-  constructor() { }
+  arrayOfPurchases=[];
+
+  constructor(
+    private _purchaseService: PurchaseService,
+    private _modalService: NgbModal) { }
 
   ngOnInit() {
+    this.getPurchases();
+  }
+
+  getPurchases(){
+    this._purchaseService.getPurchases()
+    .subscribe(res=>{
+      this.arrayOfPurchases = res;
+    });
+  }
+
+  displayTicketsInfo(id){
+    let modalRef = this._modalService.open(PurchaseInfoComponent);
+    modalRef.componentInstance.purchaseId = id;
   }
 
 }

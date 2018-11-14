c8d7b101d5d2dfbb765bcd0c050eba679390f9c3
bego <begomontes7@gmail.com>
2018-11-14 02:05:31 -0600

    Agregue notificacion y validacion a tabla de crear lugar

--- src/app/controllers/place/create-place/create-place.component.ts
+++ src/app/controllers/place/create-place/create-place.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PlaceService } from '../../../services/place.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
-
+//esto es para jquery
+declare const $: any;
 @Component({
   selector: 'app-create-place',
   templateUrl: './create-place.component.html',
@@ -19,6 +20,33 @@ export class CreatePlaceComponent implements OnInit {
     this.createForm();
   }
 
+  showNotification(data, from, align){
+    $.notify({
+        message: "Nuevo horario agregado."
+    },{
+        type: data.color,
+        timer: 1000,
+        placement: {
+            from: from,
+            align: align
+        },
+        template: `<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">
+        <button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>
+        <span data-notify="icon"></span>
+        <span data-notify="message">{2}</span>
+        <div class="progress" data-notify="progressbar">
+        <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
+        </div>
+        </div>`,
+        onShow: ()=>{
+            
+        },
+        onClose: ()=>{
+          this.router.navigate(['/place']);
+        }
+    });
+}
+
   ngOnInit() {
     this.createForm();
     this.getPlaceType();
@@ -28,6 +56,7 @@ export class CreatePlaceComponent implements OnInit {
     this.service.addPlace(name,description,longitude,latitude,place_type_id,narrative).subscribe(data => {
       this.places= data
       console.log(data);
+      this.showNotification(data, "top","left");
     });
   }
 

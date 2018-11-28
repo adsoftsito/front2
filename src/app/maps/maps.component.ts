dd661a2cf04b8f52adbee84509002dbdbe09e789
JesusLibrado <jes.librado@gmail.com>
2018-11-28 03:31:31 -0600

    Rutas v.1 ready

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { TourService } from '../services/tour.service';
+import { MatSelectChange } from '@angular/material/select';
 
 declare const google: any;
 
-interface Marker {
-lat: number;
-lng: number;
-label?: string;
-draggable?: boolean;
+class Marker {
+    public lat: number;
+    public lng: number;
+    public name?: string;
+    public draggable?: boolean;
+
+    constructor(lat: number, lng: number, name: string) {
+        this.lat = lat;
+        this.lng = lng;
+        this.name = name;
+    }
 }
 
 @Component({
@@ -16,118 +24,55 @@ draggable?: boolean;
 })
 export class MapsComponent implements OnInit {
 
-  public lat = 19.04334;
-  public lon = -98.20193;
-
-  constructor() { }
-
-  ngOnInit() {
-
+  center = {
+    lat: 19.04334,
+    lng: -98.20193
   }
 
-  /*ngOnInit() {
+  arrayOfTours = [];
+  currentTour = {
+    id: 0,
+    places: []
+  }
 
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-        zoom: 13,
-        center: myLatlng,
-        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        styles: [{
-            "featureType": "water",
-            "stylers": [{
-                "saturation": 43
-            }, {
-                "lightness": -11
-            }, {
-                "hue": "#0088ff"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "hue": "#ff0000"
-            }, {
-                "saturation": -100
-            }, {
-                "lightness": 99
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#808080"
-            }, {
-                "lightness": 54
-            }]
-        }, {
-            "featureType": "landscape.man_made",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ece2d9"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ccdca1"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "color": "#767676"
-            }]
-        }, {
-            "featureType": "road",
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "color": "#ffffff"
-            }]
-        }, {
-            "featureType": "poi",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "landscape.natural",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#b8cb93"
-            }]
-        }, {
-            "featureType": "poi.park",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.sports_complex",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.medical",
-            "stylers": [{
-                "visibility": "on"
-            }]
-        }, {
-            "featureType": "poi.business",
-            "stylers": [{
-                "visibility": "simplified"
-            }]
-        }]
+  markersOnMap: Marker[] = [];
 
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  constructor(
+      private _tourService: TourService
+  ) { }
 
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title: "Hello World!"
-    });
+  ngOnInit() {
+    this.getTours();
+  }
 
-    // To add the marker to the map, call setMap();
-    marker.setMap(map);
-  }*/
+  getTours() {
+       this._tourService.getTours()
+       .subscribe(res => {
+            this.arrayOfTours = res;
+            this.currentTour.id = this.arrayOfTours[0].id;
+            this.currentTour.places = this.arrayOfTours[0].places;
+            this.drawMarkersOnMap(this.currentTour.places);
+       });
+  }
 
+  drawMarkersOnMap(arrayOfPlaces) {
+    this.markersOnMap = [];
+    arrayOfPlaces.forEach((place) => {
+        this.markersOnMap.push(new Marker(
+            Number.parseFloat(place.latitude),
+            Number.parseFloat(place.longitude),
+            place.name
+        ));
+    })
+  }
+
+  updateCurrentTour(event: EventEmitter<MatSelectChange>) {
+    for (const tour of this.arrayOfTours) {
+        if (tour.id === event.value) {
+            this.currentTour.places = tour.places;
+            this.currentTour.id = tour.id;
+        }
+    }
+    this.drawMarkersOnMap(this.currentTour.places);
+  }
 }
